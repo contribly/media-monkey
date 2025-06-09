13c931cf7f649ed93a32c71b915208266b1d49be
Vladimir Kulev <me@lightoze.net>
2025-06-09 23:42:05 +0300

    Add prometheus metrics

---
+++ app/modules/MetricsModule.scala
@@ -0,0 +1,73 @@
+package modules
+
+import akka.stream.Materializer
+import io.micrometer.core.instrument.binder.jvm.{JvmGcMetrics, JvmMemoryMetrics, JvmThreadMetrics}
+import io.micrometer.core.instrument.{MeterRegistry, Timer}
+import io.micrometer.prometheusmetrics.{PrometheusConfig, PrometheusMeterRegistry}
+import io.prometheus.metrics.exporter.httpserver.HTTPServer
+import play.api.http.Status
+import play.api.inject.{ApplicationLifecycle, bind}
+import play.api.mvc.{Filter, RequestHeader, Result}
+import play.api.routing.Router
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class MetricsModule extends play.api.inject.SimpleModule((_,config)=>{
+  val registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+  new JvmMemoryMetrics().bindTo(registry);
+  new JvmGcMetrics().bindTo(registry);
+  new JvmThreadMetrics().bindTo(registry);
+  Seq(
+    bind(classOf[MeterRegistry]).to(registry),
+    bind(classOf[PrometheusMeterRegistry]).to(registry),
+    bind(classOf[PrometheusEndpointStarter]).toSelf.eagerly(),
+    bind(classOf[MetricsFilter]).toSelf
+  )
+})
+
+class PrometheusEndpointStarter @Inject()(lifecycle: ApplicationLifecycle, registry: PrometheusMeterRegistry) {
+  val server: HTTPServer = HTTPServer.builder()
+    .port(3000)
+    .registry(registry.getPrometheusRegistry)
+    .buildAndStart()
+
+  lifecycle.addStopHook { () =>
+    server.stop()
+    Future.successful(())
+  }
+}
+
+class MetricsFilter @Inject()(registry: MeterRegistry)(implicit val mat: Materializer, val ec: ExecutionContext) extends Filter {
+
+  private val timerProvider = Timer.builder("http.server.requests")
+    .description("HTTP server requests")
+    .publishPercentiles(0.5, 0.95, 0.99)
+    .withRegistry(registry)
+
+  override def apply(next: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] = {
+    rh.attrs.get(Router.Attrs.HandlerDef).fold {
+      next(rh)
+    } { attrs =>
+      val sample = Timer.start(registry)
+      val path = attrs.path.replaceAll("\\$(\\w+)(<[^>]+>)?", ":$1")
+      next(rh).transform(
+        result => {
+          val outcome = (result.header.status / 100) match {
+            case 2 => "SUCCESS"
+            case 3 => "REDIRECT"
+            case 4 => "CLIENT_ERROR"
+            case 5 => "SERVER_ERROR"
+            case _ => "UNKNOWN"
+          }
+          sample.stop(timerProvider.withTags("uri", path, "method", rh.method, "outcome", outcome, "status", result.header.status.toString))
+          result
+        },
+        error => {
+          sample.stop(timerProvider.withTags("uri", path, "method", rh.method, "outcome", "SERVER_ERROR", "status", Status.INTERNAL_SERVER_ERROR.toString))
+          error
+        }
+      )
+    }
+  }
+}
--- build.sbt
+++ build.sbt
@@ -2,22 +2,24 @@ name := "media-monkey"
 
 lazy val `media-monkey` = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.12.13"
+scalaVersion := "2.12.19"
 
-resolvers += "openimaj" at "http://maven.openimaj.org"
-resolvers += "billylieurance-net" at "http://www.billylieurance.net/maven2"
+resolvers += "openimaj" at "https://maven.openimaj.org"
+resolvers += "billylieurance-net" at "https://www.billylieurance.net/maven2"
 resolvers += "gael" at "https://repository.gael-systems.com/repository/public"
 
 libraryDependencies += guice
 libraryDependencies += ws
 
-libraryDependencies += "org.apache.tika" % "tika-core" % "2.9.4"
-libraryDependencies += "org.apache.tika" % "tika-parsers-standard-package" % "2.9.4"
+libraryDependencies += "org.apache.tika" % "tika-core" % "2.9.4" exclude("org.slf4j", "slf4j-api")
+libraryDependencies += "org.apache.tika" % "tika-parsers-standard-package" % "2.9.4" exclude("org.slf4j", "jcl-over-slf4j") exclude("org.slf4j", "slf4j-api")
 libraryDependencies += "org.im4java" % "im4java" % "1.4.0"
 libraryDependencies += "org.openimaj" % "core" % "1.3.6"
 libraryDependencies += "org.openimaj" % "core-image" % "1.3.6"
 libraryDependencies += "org.openimaj" % "faces" % "1.3.6"
 libraryDependencies += "commons-io" % "commons-io" % "2.5"
+libraryDependencies += "io.micrometer" % "micrometer-registry-prometheus" % "1.15.0"
+libraryDependencies += "io.prometheus" % "prometheus-metrics-exporter-httpserver" % "1.3.8"
 
 // test deps
 libraryDependencies ++= Seq(
